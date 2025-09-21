9966ec2257bd5beef8b5e51f55d4c0a7c30d0b70
Victor Opiyo <37541971+Vopiyo@users.noreply.github.com>
2025-09-21 13:47:38 +0300

    lbw_dashboard.py

--- lbw_dashboard.py
+++ lbw_dashboard.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1X3hkGWLGHMGG0J3b4uQ70eqwZsnNJlna
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import joblib
